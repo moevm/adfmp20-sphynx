f1ba35d1c9a44a30dd74667db541426ad3b495d4
Nightbot1448 <night1337bot@gmail.com>
2020-03-10 01:44:04 +0300

    fragments for bottom bar

--- app/src/main/java/com/moevm/geoquest/MainActivity.kt
+++ app/src/main/java/com/moevm/geoquest/MainActivity.kt
@@ -1,37 +1,58 @@
 package com.moevm.geoquest
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+class MainActivity : AppCompatActivity() {
 
-    private lateinit var mMap: GoogleMap
+//    private lateinit var mMap: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
 
-        startActivity(Intent(this, LoginActivity::class.java))
+        val bottomView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, QuestsFragment()).commit()
+
+        bottomView.setOnNavigationItemSelectedListener {
+            var selectedFragment: Fragment? = null
+            when (it.itemId) {
+
+                R.id.bottom_navigation_quests -> {
+                    selectedFragment = QuestsFragment()
+                    Toast.makeText(this, "quests", Toast.LENGTH_SHORT).show()
+                }
+                R.id.bottom_navigation_map -> {
+                    selectedFragment = MapFragment()
+                    Toast.makeText(this, "map", Toast.LENGTH_SHORT).show()
+                }
+                R.id.bottom_navigation_profile -> {
+                    selectedFragment = ProfileFragment()
+                    Toast.makeText(this, "profile", Toast.LENGTH_SHORT).show()
+                }
+            }
+            return@setOnNavigationItemSelectedListener if(selectedFragment != null) {
+                supportFragmentManager.beginTransaction()
+                    .replace(R.id.fragment_container, selectedFragment).commit()
+                 true
+            }
+            else
+                false
+        }
+
     }
 
 
-    override fun onMapReady(googleMap: GoogleMap) {
-        mMap = googleMap
-
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-    }
+//    override fun onMapReady(googleMap: GoogleMap) {
+//        mMap = googleMap
+//
+//        // Add a marker in Sydney and move the camera
+//        val saintP = LatLng(59.9, 31.3)
+//        mMap.addMarker(MarkerOptions().position(saintP).title("Marker in SaintP"))
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(saintP))
+//    }
 }
