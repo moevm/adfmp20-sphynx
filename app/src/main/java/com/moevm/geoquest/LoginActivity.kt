fe0426d0ac85a862a09f82c5993bdf6a8128634a
Nightbot1448 <night1337bot@gmail.com>
2020-03-11 22:23:58 +0300

    login with google

--- app/src/main/java/com/moevm/geoquest/LoginActivity.kt
+++ app/src/main/java/com/moevm/geoquest/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.moevm.geoquest
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,6 +8,8 @@ import android.util.Log
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
 import com.google.android.material.textfield.TextInputLayout
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
@@ -15,12 +18,42 @@ class LoginActivity : AppCompatActivity() {
 
     private lateinit var auth: FirebaseAuth
 
+    companion object{
+        private const val RC_SIGN_IN: Int = 123
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == Activity.RESULT_OK) {
+                // Successfully signed in
+                val user = auth.currentUser
+                Log.d("AUTHORIZATION", "user: $user")
+                updateUI(user)
+                // ...
+            } else {
+                Log.d("AUTHORIZATION", "Fail auth")
+                updateUI(null)
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+            }
+        }
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
         auth = FirebaseAuth.getInstance()
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.GoogleBuilder().build()
+        )
 
         val emailField =findViewById<TextInputLayout>(R.id.login_input).editText
         val passField = findViewById<TextInputLayout>(R.id.password_input).editText
@@ -45,16 +78,26 @@ class LoginActivity : AppCompatActivity() {
                         updateUI(null)
                     }
                 }
-
         }
 
         val signUpButton = findViewById<TextView>(R.id.sign_up_button)
         signUpButton.setOnClickListener {
             startActivity(Intent(this, RegistrationActivity::class.java))
         }
+
+        val singUnWithGoogleButton = findViewById<Button>(R.id.sign_in_google_button)
+        singUnWithGoogleButton.setOnClickListener {
+            startActivityForResult(
+                AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setAvailableProviders(providers)
+                    .build(),
+                RC_SIGN_IN)
+        }
+
     }
 
-    public override fun onStart() {
+    override fun onStart() {
         super.onStart()
         // Check if user is signed in (non-null) and update UI accordingly.
         val currentUser = auth.currentUser
