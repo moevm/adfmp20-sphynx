00a4f92310c32bee88b2db7f6247cc477cf607b3
cupofme <danil.ilyashuk@gmail.com>
2020-03-15 16:58:00 +0300

    Add info activity content

--- app/src/main/java/com/moevm/geoquest/InfoActivity.kt
+++ app/src/main/java/com/moevm/geoquest/InfoActivity.kt
@@ -2,11 +2,18 @@ package com.moevm.geoquest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class InfoActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_info)
+
+        val backButton = findViewById<ImageButton>(R.id.back_button)
+
+        backButton.setOnClickListener {
+            finish()
+        }
     }
 }
