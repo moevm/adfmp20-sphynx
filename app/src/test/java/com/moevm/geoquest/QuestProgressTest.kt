69f1b55a66ccefa06ed8dce224e693eeafbca366
Dmitry Ivanov <darcenrall@gmail.com>
2020-04-17 16:27:29 +0300

    add @before for test

--- app/src/test/java/com/moevm/geoquest/QuestProgressTest.kt
+++ app/src/test/java/com/moevm/geoquest/QuestProgressTest.kt
@@ -2,8 +2,11 @@ package com.moevm.geoquest
 
 import android.location.Location
 import android.location.LocationManager
+import com.google.android.gms.maps.model.LatLng
+import com.moevm.geoquest.models.AttractionModel
 import com.moevm.geoquest.models.AttractionStatus
 import org.junit.Assert.assertEquals
+import org.junit.Before
 import org.junit.Test
 
 
@@ -11,9 +14,14 @@ class QuestProgressTest {
 
     lateinit var progress: QuestProgress
 
+    @Before
+    fun init() {
+        progress = QuestProgress()
+    }
+    
     @Test
     fun emptyQuestProject(){
-        val progress = QuestProgress()
+        progress = QuestProgress()
         assertEquals(progress.getQuestAttractionStartCount(), 0)
         assertEquals(progress.getLastFounded(), null)
         assertEquals(progress.getTravelledDistance(), 0.0, 0.001)
