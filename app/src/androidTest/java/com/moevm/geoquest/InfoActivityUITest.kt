722009642b41bfe0273d4a2f6d004766b14e8de1
Dmitry Ivanov <darcenrall@gmail.com>
2020-04-18 21:41:05 +0300

    add basic test class w/ rule for info activity

---
+++ app/src/androidTest/java/com/moevm/geoquest/InfoActivityUITest.kt
@@ -0,0 +1,13 @@
+package com.moevm.geoquest
+
+import androidx.test.rule.ActivityTestRule
+import org.junit.Rule
+
+
+class InfoActivityUITest {
+
+    @get:Rule
+    var activityRule: ActivityTestRule<InfoActivity>
+            = ActivityTestRule(InfoActivity::class.java)
+
+}
