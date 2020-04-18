ec4e797395326442622a9e84fa8b289d62fad186
Dmitry Ivanov <darcenrall@gmail.com>
2020-04-19 02:00:03 +0300

    add simple test for main activity

---
+++ app/src/androidTest/java/com/moevm/geoquest/MainActivityUITest.kt
@@ -0,0 +1,48 @@
+package com.moevm.geoquest
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.rule.ActivityTestRule
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.contrib.java.lang.system.EnvironmentVariables
+
+
+class MainActivityUITest {
+
+    @get:Rule
+    var activityRule: ActivityTestRule<MainActivity>
+            = ActivityTestRule(MainActivity::class.java)
+    private val environmentVariables = EnvironmentVariables()
+
+
+    @Before
+    fun setUp(){
+        environmentVariables.set("test_mode", "true")
+    }
+
+
+    @After
+    fun tearDown(){
+        environmentVariables.set("test_mode", "")
+    }
+
+    @Test
+    fun test_elementsIsDisplayed() {
+        onView(withId(R.id.bottom_navigation))
+            .check(matches(isDisplayed()))
+        onView(withId(R.id.bottom_navigation_quests))
+            .check(matches(isDisplayed()))
+        onView(withId(R.id.bottom_navigation_map))
+            .check(matches(isDisplayed()))
+        onView(withId(R.id.bottom_navigation_profile))
+            .check(matches(isDisplayed()))
+        onView(withId(R.id.fragment_container))
+            .check(matches(isDisplayed()))
+    }
+
+}
