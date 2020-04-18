b4bfd381e1c2797de69030d61d64b2c748834b47
Dmitry Ivanov <darcenrall@gmail.com>
2020-04-18 21:43:29 +0300

    add test for check title

--- app/src/androidTest/java/com/moevm/geoquest/InfoActivityUITest.kt
+++ app/src/androidTest/java/com/moevm/geoquest/InfoActivityUITest.kt
@@ -1,7 +1,11 @@
 package com.moevm.geoquest
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.rule.ActivityTestRule
 import org.junit.Rule
+import org.junit.Test
 
 
 class InfoActivityUITest {
@@ -9,5 +13,12 @@ class InfoActivityUITest {
     @get:Rule
     var activityRule: ActivityTestRule<InfoActivity>
             = ActivityTestRule(InfoActivity::class.java)
+
+    @Test
+    fun test_TitleIsDisplayed() {
+        onView(withId(R.id.title))
+            .check(matches(isDisplayed()))
+            .check(matches(withText("Как играть?")))
+    }
 
 }
