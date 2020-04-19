bab477153cf2eb34a1901bb745afce93564b72fe
Dmitry Ivanov <darcenrall@gmail.com>
2020-04-19 17:26:15 +0300

    add test for InfoActivity

---
+++ app/src/androidTest/java/com/moevm/geoquest/InfoActivityUITest.kt
@@ -0,0 +1,40 @@
+package com.moevm.geoquest
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.rule.ActivityTestRule
+import org.junit.Rule
+import org.junit.Test
+
+
+class InfoActivityUITest {
+
+    @get:Rule
+    var activityRule: ActivityTestRule<InfoActivity>
+            = ActivityTestRule(InfoActivity::class.java)
+
+    @Test
+    fun test_TitleIsDisplayed() {
+        onView(withId(R.id.title))
+            .check(matches(isDisplayed()))
+            .check(matches(withText("Как играть?")))
+    }
+
+
+    @Test
+    fun test_InfoIsDisplayed() {
+        onView(withId(R.id.text_info))
+            .check(matches(isDisplayed()))
+    }
+
+
+    @Test
+    fun test_backButton() {
+        onView(withId(R.id.back_button))
+            .check(matches(isDisplayed()))
+            .perform(click())
+    }
+
+}
